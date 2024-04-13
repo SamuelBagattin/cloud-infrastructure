ddd4e03e4dd754f0d8beba716fa9f19ad0ac9116
Samuel BAGATTIN <42091952+SamuelBagattin@users.noreply.github.com>
2024-04-13 16:15:38 +0200

    pipeline

--- github_actions_oidc.tf
+++ github_actions_oidc.tf
@@ -5,6 +5,7 @@ module "aws_github_actions_oidc" {
     "SamuelBagattin" : {
       role_name = "githubActions-role"
       allowed_branches = ["*"]
+      allowed_environments = ["*"]
       pull_requests = true
       repositories = {
         "cloud-infrastructure" = {}
